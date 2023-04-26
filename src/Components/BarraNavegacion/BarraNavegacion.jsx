8a6fc27be231276c4a115875509340f52ec00aff
lucasmartinmore <lucasmartinmore@hotmail.com>
2023-04-26 00:49:59 -0300

    Agregados

--- src/Components/BarraNavegacion/BarraNavegacion.jsx
+++ src/Components/BarraNavegacion/BarraNavegacion.jsx
@@ -3,16 +3,19 @@ import './BarraNavegacion.css'
 
 const BarraNavegacion = () => {
   return (
+    //Agregar hreaf a la etiqueta "a" para hacer referencias
     <header>
     <h2 style={{color:"black"}}>FUNKO STORE</h2>
+    <div className='MenuLateral'>
     <nav>
         <ul>
-        <li>Marvel</li>
-        <li>Dc</li>
-        <li>Peliculas</li>
-        <li>Disney</li>
+        <li id='item1'><a>Marvel</a></li> 
+        <li id='item2'><a >Dc</a></li>
+        <li id='item3'><a >Peliculas</a></li>
+        <li id='item4'><a >Disney</a></li>
         </ul>
     </nav>
+    </div>
     <CarritoDeCompra/>
     </header>
   )
